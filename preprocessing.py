fd9e04280409448f68f3d048c54e90a2eb6573bb
daoducminh <daoducminh1997@gmail.com>
2021-05-25 11:44:47 +0700

    Update preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -5,52 +5,6 @@ from sklearn.compose import ColumnTransformer
 
 fn = 'ibm_hr/{}.pkl'
 
-RAW_VALUES = {
-    'Education': {
-        1: 'Below College',
-        2: 'College',
-        3: 'Bachelor',
-        4: 'Master',
-        5: 'Doctor'
-    },
-    'EnvironmentSatisfaction': {
-        1: 'Low',
-        2: 'Medium',
-        3: 'High',
-        4: 'Very High'
-    },
-    'JobInvolvement': {
-        1: 'Low',
-        2: 'Medium',
-        3: 'High',
-        4: 'Very High'
-    },
-    'JobSatisfaction': {
-        1: 'Low',
-        2: 'Medium',
-        3: 'High',
-        4: 'Very High'
-    },
-    'PerformanceRating': {
-        1: 'Low',
-        2: 'Good',
-        3: 'Excellent',
-        4: 'Outstanding'
-    },
-    'RelationshipSatisfaction': {
-        1: 'Low',
-        2: 'Medium',
-        3: 'High',
-        4: 'Very High'
-    },
-    'WorkLifeBalance': {
-        1: 'Bad',
-        2: 'Good',
-        3: 'Better',
-        4: 'Best'
-    },
-}
-
 
 def read_pickle(filename):
     with open(filename, 'rb') as f:
@@ -70,9 +24,6 @@ if __name__ == '__main__':
         'Over18',
         'EmployeeNumber'
     ])
-    # df['PerformanceRating'] = df['PerformanceRating'].astype(str)
-    for c in RAW_VALUES:
-        df[c] = df[c].apply(lambda x: RAW_VALUES[c][x])
     encoder = LabelEncoder()
     df['Attrition'] = encoder.fit_transform(df['Attrition'])
     categorical_features = []
